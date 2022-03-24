449d7606c67231572e3d3bc7bf4c7180d2507983
97hackbrian <brayandurantoconas@gmail.com>
2022-03-24 00:11:48 -0400

    v1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <termios.h>    //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>     //STDIN_FILENO
+
+
+
+#include <math.h>
+//#include "portaudio.h"
+#include <stdint.h>
+#include <unistd.h> // for usleep()
